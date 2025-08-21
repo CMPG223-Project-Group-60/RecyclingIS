e7702ef83e01a22540fc5d4deeb2ad76e221d819
LeeSeatile <seatilejm@gmail.com>
2025-08-21 22:04:12 +0200

    Merit form done
    Added gui to the merit form
    
    Co-Authored-By: mmusorams <144959178+mmusorams@users.noreply.github.com>

--- RecyclingIS/RecyclingIS/frmMeritManagement.Designer.cs
+++ RecyclingIS/RecyclingIS/frmMeritManagement.Designer.cs
@@ -29,9 +29,17 @@ namespace RecyclingIS
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMeritManagement));
             this.lblHeading = new System.Windows.Forms.Label();
             this.lblSubHeading = new System.Windows.Forms.Label();
-            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnAllocateMerit = new System.Windows.Forms.Button();
+            this.pnlSearch = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dgvDisplayStudentMerits = new System.Windows.Forms.DataGridView();
+            this.pnlSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplayStudentMerits)).BeginInit();
             this.SuspendLayout();
             // 
             // lblHeading
@@ -58,28 +66,80 @@ namespace RecyclingIS
             this.lblSubHeading.TabIndex = 3;
             this.lblSubHeading.Text = "Manage the master list of recyclable item types";
             // 
-            // btnAddStudent
+            // btnAllocateMerit
             // 
-            this.btnAddStudent.BackColor = System.Drawing.Color.ForestGreen;
-            this.btnAddStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddStudent.ForeColor = System.Drawing.Color.White;
-            this.btnAddStudent.Location = new System.Drawing.Point(626, 81);
-            this.btnAddStudent.Name = "btnAddStudent";
-            this.btnAddStudent.Size = new System.Drawing.Size(162, 55);
-            this.btnAddStudent.TabIndex = 4;
-            this.btnAddStudent.Text = "Add Student";
-            this.btnAddStudent.UseVisualStyleBackColor = false;
+            this.btnAllocateMerit.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnAllocateMerit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAllocateMerit.ForeColor = System.Drawing.Color.White;
+            this.btnAllocateMerit.Location = new System.Drawing.Point(756, 84);
+            this.btnAllocateMerit.Name = "btnAllocateMerit";
+            this.btnAllocateMerit.Size = new System.Drawing.Size(208, 55);
+            this.btnAllocateMerit.TabIndex = 4;
+            this.btnAllocateMerit.Text = "Allocate Merit To Student";
+            this.btnAllocateMerit.UseVisualStyleBackColor = false;
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.BackColor = System.Drawing.Color.Transparent;
+            this.pnlSearch.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlSearch.Controls.Add(this.pictureBox1);
+            this.pnlSearch.Controls.Add(this.txtSearch);
+            this.pnlSearch.Location = new System.Drawing.Point(3, 155);
+            this.pnlSearch.Name = "pnlSearch";
+            this.pnlSearch.Padding = new System.Windows.Forms.Padding(1);
+            this.pnlSearch.Size = new System.Drawing.Size(961, 47);
+            this.pnlSearch.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.ForeColor = System.Drawing.Color.DarkGray;
+            this.txtSearch.Location = new System.Drawing.Point(55, 4);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(904, 35);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TabStop = false;
+            // 
+            // dgvDisplayStudentMerits
+            // 
+            this.dgvDisplayStudentMerits.AllowUserToAddRows = false;
+            this.dgvDisplayStudentMerits.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDisplayStudentMerits.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplayStudentMerits.Location = new System.Drawing.Point(30, 232);
+            this.dgvDisplayStudentMerits.Name = "dgvDisplayStudentMerits";
+            this.dgvDisplayStudentMerits.RowHeadersWidth = 51;
+            this.dgvDisplayStudentMerits.RowTemplate.Height = 24;
+            this.dgvDisplayStudentMerits.Size = new System.Drawing.Size(920, 249);
+            this.dgvDisplayStudentMerits.TabIndex = 9;
             // 
             // frmMeritManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnAddStudent);
+            this.ClientSize = new System.Drawing.Size(1011, 517);
+            this.Controls.Add(this.dgvDisplayStudentMerits);
+            this.Controls.Add(this.pnlSearch);
+            this.Controls.Add(this.btnAllocateMerit);
             this.Controls.Add(this.lblSubHeading);
             this.Controls.Add(this.lblHeading);
             this.Name = "frmMeritManagement";
             this.Text = "frmMeritManagement";
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplayStudentMerits)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +149,10 @@ namespace RecyclingIS
 
         private System.Windows.Forms.Label lblHeading;
         private System.Windows.Forms.Label lblSubHeading;
-        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnAllocateMerit;
+        private System.Windows.Forms.Panel pnlSearch;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvDisplayStudentMerits;
     }
 }
