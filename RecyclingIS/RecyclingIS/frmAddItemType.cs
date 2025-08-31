3430fce7b3eabaef8a5792eca65b27a82ef20b39
Gh0st-y <ruanvandermerwe208@gmail.com>
2025-09-01 00:38:28 +0200

    add item form code

--- RecyclingIS/RecyclingIS/frmAddItemType.cs
+++ RecyclingIS/RecyclingIS/frmAddItemType.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace RecyclingIS
 {
     public partial class frmAddItemType : Form
     {
+        private string connectionString = @"";
+        private SqlConnection con;
+
         public frmAddItemType()
         {
             InitializeComponent();
@@ -22,9 +26,31 @@ namespace RecyclingIS
             this.Close();
         }
 
-        private void frmAddItemType_Load(object sender, EventArgs e)
+        private void btnAddStudent_Click(object sender, EventArgs e)
         {
+            con = new SqlConnection(connectionString);
+            SqlCommand cmd;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            string sql;
 
+            con.Open(); // Open database connection
+
+            sql = "INSERT INTO ITEM VALUES(" + txtItemName.Text + ", " + cbxQty.SelectedItem.ToString() + ")";
+            cmd = new SqlCommand(sql, con);
+
+            try
+            {
+                adapter.UpdateCommand = cmd;
+                adapter.UpdateCommand.ExecuteNonQuery();
+            } catch (SqlException error)
+            {
+                MessageBox.Show("Item couldn't be added!");
+            }
+
+            cmd.Dispose();
+            con.Close(); // Close database connection
+
+            MessageBox.Show("Item added succesfully!");
         }
     }
 }
